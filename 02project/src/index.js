27b15599f6f4b2312c11c9141552b08614a74f9e
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-10-08 22:39:39 +0530

    updated express

--- 02project/src/index.js
+++ 02project/src/index.js
@@ -1,13 +1,27 @@
 import mongoose from 'mongoose'
+import { app } from './app.js';
 import express from 'express'
 import { DB_NAME } from './constants.js';
 import connectDB from './db/db.js';
 
 
-const app = express()
-const PORT = process.env.PORT
+//const app = express()
+const PORT = process.env.PORT || 8080
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server listening at http://localhost:${PORT}`)
+    })
+})
+.catch((error)=> {
+    console.log("ERROR: ", error)
+})
+
+
+
+
+
 
 /*
 ;( async ()=>{
