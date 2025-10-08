1d7dd8d7a3efa7ed670d7652b6d17d8be0ffd52f
Rohit Kumar <contactme.rohitkumar@gmail.com>
2025-10-08 15:30:16 +0530

    updated

--- 01dataModelling/models/ecommerce/categoryModels.js
+++ 01dataModelling/models/ecommerce/categoryModels.js
@@ -4,31 +4,6 @@ const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    productImage: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        default: 0
-    },
-    stock: {
-        default: 0,
-        type: Number
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: true
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
     }
 }, {timestamps: true})
 
--- 01dataModelling/models/ecommerce/orderModels.js
+++ 01dataModelling/models/ecommerce/orderModels.js
@@ -1,5 +1,16 @@
 import mongoose, { mongo } from "mongoose";
 
+const orderItemSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    quantity: {
+        type: Number,
+        required: true
+    }
+})
+
 const orderSchema = new mongoose.Schema({
     orderPrice: {
         type: Number,
@@ -11,7 +22,16 @@ const orderSchema = new mongoose.Schema({
         ref: "User"
     },
     orderItems: {
-        type: []
+        type: [orderItemSchema]
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ["PENDING","CANCELLED","DELIVERED"],
+        default: "PENDING"
     }
 }, {timestamps: true})
 
